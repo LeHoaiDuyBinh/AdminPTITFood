d580b838242161a1f783dc3d3a848647e4c8e4f3
ToBi <lehoaiduybinh.pch.2b7.97@gmail.com>
2024-04-04 10:21:54 +0700

    Add model admin and Create User Admin new in AddUserAdmin Activity

---
+++ app/src/main/java/com/example/ptitfoodadmin/model/AdminModel.kt
@@ -0,0 +1,11 @@
+package com.example.ptitfoodadmin.model
+
+data class AdminModel(
+    val adminCode: String?= null,
+    val name: String?= null,
+    val email: String?= null,
+    val password: String?= null,
+    val role: String?= null,
+    val address: String?= null,
+    val phone: String?= null
+)
